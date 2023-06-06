203c4b6822e84c297a30265a287b5fcbf88dcfc7
Ansar Ismagilov <ansar89kab@bk.ru>
2023-06-06 11:54:13 +0800

    field injections

--- src/main/java/com/kabgig/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/kabgig/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -1,10 +1,35 @@
 package com.kabgig.learnspringframework.examples.a1;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
+
+@Component
+class YourBusinessClass{
+    @Autowired //field injection
+    Dependency1 dependency1;
+    @Autowired //field injection
+    Dependency2 dependency2;
+
+    public String toString(){
+        return "Using " + dependency1 + " and \n"+ dependency2;
+    }
+
+}
+
+@Component
+class Dependency1{
+
+}
+
+@Component
+class Dependency2{
+
+}
 
 @Configuration
 @ComponentScan
@@ -17,6 +42,7 @@ public class DepInjectionLauncherApplication {
         ) {
             Arrays.stream(context.getBeanDefinitionNames())
                     .forEach(System.out::println);
+            System.out.println(context.getBean(YourBusinessClass.class));
         }
 
     }
